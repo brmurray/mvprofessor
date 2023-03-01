4cae586a41e2839775d6e8fd7d477202371d5a2f
brmurray <brmurray@vt.edu>
2023-03-01 14:05:00 +0100

    re-seed node_finder DFS

--- scripts/node_finder.py
+++ scripts/node_finder.py
@@ -23,13 +23,16 @@ from geographiclib.geodesic import Geodesic
 from mvprofessor.config import int_data_dir
 from mvprofessor.custom_funcs import get_endpoints, make_blobs
 
-
+# Import the Professor feeder linestrings, and exclude the tiniest segments
 gdf = pd.read_pickle(int_data_dir/'professor.pkl')
+gdf = gdf[gdf['SHAPE__Length'] > 30] #with cutoff=20, len(gdf)=197
 
-# exclude tiniest linestrings from geodataframe
-gdf = gdf[gdf['SHAPE__Length'] > 50] #with cutoff=20, len(gdf)=197
+# Create other layers: linestring endpoints (pts) and combined buffers (blobs)
 pts = get_endpoints(gdf)
-blobs = make_blobs(pts,6)
+blobs = make_blobs(pts,10)
+blobs['powered'] = 0
+
+# Points of Interest 
 poi = pd.read_pickle(int_data_dir/'PoI_Professor.pkl')
 
 
@@ -37,70 +40,95 @@ poi = pd.read_pickle(int_data_dir/'PoI_Professor.pkl')
 isla_vista = poi.loc[poi['PoI']=='isla_vista_ss']['geometry']
 isla_vista = isla_vista.iloc[0]
 
-blob0 = blobs.sindex.nearest(isla_vista)[1][0] #index to nearest blob from IVSS
+root_idx = blobs.sindex.nearest(isla_vista)[1][0] #index to nearest blob from IVSS
 
-root_blob = blobs.iloc[blob0]['geometry'] # a shapely.Polygon
+root_blob = blobs.iloc[root_idx]['geometry'] # a shapely.Polygon
 root_pt= root_blob.representative_point() #shapely.Point for pos arg to nx node
 
 #%% Tree Growing Algorithm
 # Inspired by a classic depth-first search (DFS) tree traversal
 #
-# This method current relies on an apriori list of the blobs
-# Improve it by using sindex.nearest() for each distal linestring endpoint to
-# create the blobs iteratively
+# Beginning from a seed node, the inner "while" loop will conduct a DFS
+# tree traversal to connect all the line segments sharing common "blobs"
+# ("blobs" are buffered endpoints of each line segment, combined into polygons
+# where the buffers overlap).
+#
+# The outer "while" loop re-seeds the DFS algorithm using a random blob from
+# among the non-yet-connected blobs. This is necessary because the input line 
+# segments (GIS data) may be imprecise, i.e., two endpoints may be electrially
+# connected but separated geographically by more than the 2x buffer radius. 
+# The opposite can also be true, where two points are close but not necessarily
+# electrically connected. 
 
 # Initiate graph and add first point
 G = nx.Graph()
-G.add_node(blob0,pos=root_pt.coords[0],blob=root_blob)
+G.add_node(root_idx,pos=root_pt.coords[0],blob=root_blob)
+blobs.loc[root_idx,'powered']=1
 
 # These are used for the DFS iteration
-frontier = [blob0] # initiate frontier with the root node
+frontier = [root_idx] # initiate frontier with the root node
 gdf['explored'] = 0
 gdf['leaf'] = 0
 explored = []
 
-while len(frontier)>0:
-    current_node = frontier.pop(-1) # for DFS, frontier is a STACK (LIFO)
-    
-    # Find segments which connect to the current blob
-    blob = G.nodes[current_node]['blob']
-    branches = gdf.loc[gdf.intersects(blob,align=True)] # returns a dataframe
-    branches = branches[branches.explored !=1]  
-    
+# outer loop: re-seed the DFS algorithm if the network is discontinous
+while not np.all(blobs['powered']):
+    if len(frontier)==0:
         
-    # Each branch (linestring) has exactly two endpoints (boundaries)
-    # Find which endpoint is within the buffer, and which is the next node
-    for idx,branch in branches.iterrows():
+        powered_blobs = blobs[blobs['powered']==1]
+        unpowered_blobs = blobs[blobs['powered']==0]
+        reinit_blob = blobs[blobs['powered']==0].iloc[0]['geometry']
+        reinit_pt = reinit_blob.representative_point()
         
-        pt0 = Point(branch.geometry.coords[0])
-        pt1 = Point(branch.geometry.coords[-1])
+        frontier.append(blobs[blobs['powered']==0].iloc[0]['blob_idx'])
         
-        if blob.covers(pt0):
-            new_point = pt1
-        else:
-            new_point = pt0
+        G.add_node(frontier[0],pos=reinit_pt.coords[0],blob=reinit_blob)
+        blobs.loc[frontier[0],'powered']=1
+
+    # inner loop: DFS traversal of node connected by line segments
+    while len(frontier)>0:
+        current_node = frontier.pop(-1) # for DFS, frontier is a STACK (LIFO)
         
-        new_blob = blobs[blobs.covers(new_point)]
-                
-        # Add the new node to the graph and to the frontier
-        G.add_node(new_blob.index[0], 
-                   pos=(new_blob.representative_point().iloc[0].coords[0]),
-                   blob=new_blob.iloc[0]['geometry'])
-        frontier.append(new_blob.index[0]) # we'll revisit this point later
+        # Find segments which connect to the current blob
+        blob = G.nodes[current_node]['blob']
+        branches = gdf.loc[gdf.intersects(blob,align=True)] # returns a dataframe
+        branches = branches[branches.explored !=1]  
         
-        # Add the edge from first node to new node
-        # The entire branch (pd.Series) is added to the node
-        # All the branch elements are also added for easy access later
-        G.add_edge(current_node,new_blob.index[0],weight=branch.SHAPE__Length,
-                       length=branch.SHAPE__Length,
-                       section_id=branch.name,
-                       objectid=branch.objectid,
-                       node_id=branch.node_id,
-                       geometry=branch.geometry,
-                       branch=branch)
-        
-        # Note which branches have been mapped, so as not repeat them
-        gdf.loc[idx,'explored'] = 1
+            
+        # Each branch (linestring) has exactly two endpoints (boundaries)
+        # Find which endpoint is within the buffer, and which is the next node
+        for idx,branch in branches.iterrows():
+            
+            pt0 = Point(branch.geometry.coords[0])
+            pt1 = Point(branch.geometry.coords[-1])
+            
+            if blob.covers(pt0):
+                new_point = pt1
+            else:
+                new_point = pt0
+            
+            new_blob = blobs[blobs.covers(new_point)]
+                    
+            # Add the new node to the graph and to the frontier
+            G.add_node(new_blob.index[0], 
+                       pos=(new_blob.representative_point().iloc[0].coords[0]),
+                       blob=new_blob.iloc[0]['geometry'])
+            frontier.append(new_blob.index[0]) # we'll revisit this point later
+            
+            # Add the edge from first node to new node
+            # The entire branch (pd.Series) is added to the node
+            # All the branch elements are also added for easy access later
+            G.add_edge(current_node,new_blob.index[0],weight=branch.SHAPE__Length,
+                           length=branch.SHAPE__Length,
+                           section_id=branch.name,
+                           objectid=branch.objectid,
+                           node_id=branch.node_id,
+                           geometry=branch.geometry,
+                           branch=branch)
+            
+            # Note which branches and blobs have been mapped
+            gdf.loc[idx,'explored'] = 1
+            blobs.loc[new_blob.index,'powered']=1
     
 #%% Make the electrical nodes (enodes) into a GDF
 pos = nx.get_node_attributes(G,'pos')
@@ -109,17 +137,37 @@ x,y = list(zip(*list(pos.values())))
 
 enode_pts = geopandas.GeoSeries.from_xy(x,y,crs="EPSG:2955")
 enodes = geopandas.GeoDataFrame({'enode_id':enode_id},geometry=enode_pts,crs="EPSG:2955")
+enodes = enodes.set_index('enode_id')
+
+#%% Flag the electrical nodes by their subgraph
+# add a random number column for coloring
+S = [G.subgraph(c).copy() for c in nx.connected_components(G)]
+
+enodes['subgraph'] = 0
+enodes['randc'] = 0
+rng = default_rng()
+r = rng.choice(len(S)*2,size=len(S),replace=False)
+for i,Si in enumerate(S):
+    enodes.loc[enodes.index.isin(list(Si.nodes)),'subgraph']=i
+    enodes.loc[enodes.index.isin(list(Si.nodes)),'randc']=r[i]
+    
 
 #%% Plot the DRPEP lines, blobs, and electrical nodes
-m = gdf.explore(column='randc',cmap='gist_rainbow',legend=False,
-                name="DRPEP Line Sections")
+# m = gdf.explore(column='randc',cmap='gist_rainbow',legend=False,
+#                 name="DRPEP Line Sections")
+m=gdf.explore(name="DRPEP Line Sections")
 blobs.explore(m=m,color='green',marker_type='circle',
-              style_kwds=dict(fillOpacity=0.2),
+              style_kwds=dict(fillOpacity=0.2),show=False,
+              
               name="Blobs (initial guess nodes)")
-enodes.explore(m=m,color='red',marker_kwds=dict(radius=5),
-               name="Electrical nodes (programmatic)")
-pts.explore(m=m,color='blue',marker_kwds=dict(radius=2),
+enodes.explore(m=m,column='randc',cmap='prism',marker_kwds=dict(radius=7),
+                legend=False,style_kwds=dict(fillOpacity=1.0),
+                name="Electrical Nodes (colored by sub-network)")
+pts.explore(m=m,color='blue',marker_kwds=dict(radius=2),show=False,
                name="LineString endpoints")
 poi.explore(m=m,color='green',marker_type='marker',name="Points of Interest")
 folium.LayerControl().add_to(m)
 m.save('enodes.html')
+
+
+
